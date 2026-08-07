cedb2ad56a3f6588474d498dcdefa6342594ca8a
Adnan Raad <adnanraad2002@gmail.com>
2026-08-07 22:41:03 +0600

    updated

--- main.cpp
+++ main.cpp
@@ -126,7 +126,7 @@ void planeWindow(float x, float y)
     glEnd();
 }
 
-void planeCircle(float x, float y)
+void planeDoorCircle(float x, float y)
 {
     float radius = 0.796f;
 
@@ -143,6 +143,31 @@ void planeCircle(float x, float y)
             glVertex2f(
                 x + radius * cos(angle),
                 y + radius * sin(angle)
+            );
+        }
+
+    glEnd();
+}
+
+void circle(float centerX, float centerY, float radiusX, float radiusY)
+{
+    // Calculate radius from the two points
+    float radius = sqrt(
+        (radiusX - centerX) * (radiusX - centerX) +
+        (radiusY - centerY) * (radiusY - centerY)
+    );
+
+    glBegin(GL_TRIANGLE_FAN);
+
+        glVertex2f(centerX, centerY);
+
+        for (int i = 0; i <= 100; i++)
+        {
+            float angle = 2.0f * 3.1416f * i / 100;
+
+            glVertex2f(
+                centerX + radius * cos(angle),
+                centerY + radius * sin(angle)
             );
         }
 
@@ -262,7 +287,37 @@ void runwayPlane()
      glVertex2f(28.804627368261, 16.032099651688);
      glVertex2f(28.804627368261, 20.032099651688);
      glEnd();
-     planeCircle(27.8046, 18.5321);
+     planeDoorCircle(27.8046, 18.5321);
+
+     //plane front wheel
+     glColor3ub(0,0,0);
+     glBegin(GL_QUADS);
+     glVertex2f(31.3833324793164, 12.5928111121142);
+     glVertex2f(31.994050076722, 12.6033407258625);
+     glVertex2f(31.994050076722, 14.5324931212866);
+     glVertex2f(31.3833324793164,14.5324931212866);
+     glEnd();
+     circle(31.6535964332166, 12.2923002908545,31.8218151473123, 12.0318326045127);
+
+     //plane back wheel left
+     glColor3ub(0,0,0);
+     glBegin(GL_QUADS);
+     glVertex2f(13.2029509052645, 12.3746674003598);
+     glVertex2f(14.0041352940111, 12.3866253763112);
+     glVertex2f(14.0041352940111, 14.5323931212866);
+     glVertex2f(13.2029509052645, 14.5323931212866);
+     glEnd();
+     circle(13.4790138073317, 11.8715738633303,13.8182001946567, 11.49325058516);
+
+     //plane back wheel right
+     glColor3ub(0,0,0);
+     glBegin(GL_QUADS);
+     glVertex2f(14.3902056493907, 13.017790945389);
+     glVertex2f(14.8133426700766, 13.017790945389);
+     glVertex2f(14.8133426700766, 14.5324931212866);
+     glVertex2f(14.3902056493907, 14.5324931212866);
+     glEnd();
+     circle(14.6009380115608, 12.5499466379804,14.783576835505, 12.1194408386832);
 
 }
 
